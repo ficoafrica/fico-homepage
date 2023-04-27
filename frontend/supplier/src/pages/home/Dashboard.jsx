96e37afdd8f6ffd6ec041d6e643127bd1534336e
unknown <harryd57@yahoo.com>
2023-04-27 02:45:38 +0100

    removed vendor, supplier, super-admin folder from repo

--- frontend/supplier/src/pages/home/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Dashboard = () => {
-  return (
-    <div>Supplier Dashboard</div>
-  )
-}
-
-export default Dashboard
--- frontend/vendor/src/pages/home/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Dashboard = () => {
-  return (
-    <div>Vendor Dashboard</div>
-  )
-}
-
-export default Dashboard
