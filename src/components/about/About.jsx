bc4259cb552b966697786f270251e42f503f94c4
davis <awoingodavies@gmail.com>
2023-08-16 11:14:47 +0100

    Text and some changes

---
+++ src/components/about/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='text-center max-w-[1280px] mx-auto' > 
+            <h1 className='text-2xl font-bold text-gray-800' > About Us </h1>
+            <p>Fico Africa is an AgTech startup that focuses on providing local farmers access to
+                a large and lucrative vendor market. 
+                We also help farmers transition into automated farm technologies like Climate Smart Agriculture, 
+                and Precision Farming which enables them increase their efficiency and productivity to yeild more income. 
+                We aim to tackle the issue of food insecurity and inflation by providing quality fresh farm products 
+                at affordable price for our vendors, with a safe and secure farm logistics system. 
+            </p>
+        </div>
+    )
+}
+
+export default About;
