dba65c56b8546039368be4e6d2c0e80de7973150
harryd57 <harryd57@yahoo.com>
2023-08-22 10:31:34 +0100

    Homepage update 60% complete

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -7,18 +7,14 @@ import img3 from '../../img/home/img3.png';
 const Intro = () => {
   return (
     <section className='lg:h-screen max-w-[1280px] lg:max-w-full border-b-2 bg-[#43a047]'>
-      <div className='bg-white h-screen py-8 rounded-bl-[300px] rounded-tr-[300px] hidden lg:block'>
-        <div className='grid grid-cols-2 max-w-[1020px] h-[400px] mx-auto'>
-          <div className='mt-8'>
+      <div className='bg-white h-screen py-8 rounded-bl-[180px] rounded-tr-[180px] hidden lg:block'>
+        <div className='mt-8 grid grid-cols-2 max-w-[1150px] h-[400px] mx-auto'>
+          <div className='mt-8 w-[500px]'>
             <h2 className='text-3xl font-bold text-gray-800'>Fico Africa</h2>
-            <p className='my-8 text-sm text-gray-800 pr-4'>Fico Africa is an AgTech startup
-                that focuses on giving
-                smallholder farmers from underserved communities
-                access to a large network of global and local vendors
-                market. We aim to solve the problem of food insecurity and reduce inflation
-                by providing faarmers with enabling technologies like Climate Smart Agriculture and Precision Farming. 
-                We also provide secure logistics for vendors
-                through our logistics solution built for the African market.</p>
+            <p className='my-8 text-sm text-gray-800 pr-4'>A technology based supply chain company, that
+                    focuses on giving smallholder farmers from underserved
+                    communities access to a large network of global and
+                    local vendors market.</p>
             <div className='flex justify-between w-[330px]'>
               <span>
               <button className='h-[40px] w-[150px] bg-[#43a047] border rounded-bl-[15px] rounded-tr-[15px] text-gray-100'>Vendors</button>
@@ -28,22 +24,22 @@ const Intro = () => {
               </span>
             </div>
           </div>
-          <div>
-            <div id='image'>
+          <div className='py-4'>
+            <div id='image' className='w-[480px] mx-auto'>
               <img src={union} alt='union'/>
             </div>
           </div>
         </div>
 
-        <div id='round-image' className='w-[400px] mx-auto flex justify-between'>
+        <div id='round-image' className='w-[400px] mt-4 mx-auto flex justify-between'>
           <span>
-            <img src={img1} alt='image1'/>
+            <img className='h-[80px]' src={img1} alt='image1'/>
           </span>
           <span>
-            <img src={img2} alt='image2'/>
+            <img className='h-[80px]' src={img2} alt='image2'/>
           </span>
           <span>
-            <img src={img3} alt='image3'/>
+            <img className='h-[80px]' src={img3} alt='image3'/>
           </span>
         </div>
       </div>
@@ -51,20 +47,16 @@ const Intro = () => {
       <div className='bg-white py-6 rounded-bl-[120px] rounded-tr-[120px] lg:hidden'>
         <div className='text-center w-[350px] mx-auto'>
         <h2 className='text-2xl font-bold text-gray-800'>Fico Africa</h2>
-            <p className='my-8 text-sm text-gray-800 pr-4'>Fico Africa is an AgTech startup
-                that focuses on giving
-                smallholder farmers from underserved communities
-                access to a large network of global and local vendors
-                market. We aim to solve the problem of food insecurity and reduce inflation
-                by providing faarmers with enabling technologies like Climate Smart Agriculture and Precision Farming. 
-                We also provide secure logistics for vendors
-                through our logistics solution built for the African market.</p>
+            <p className='mt-8 mb-10 text-sm text-gray-800 pr-4'>A technology based supply chain company, that
+                    focuses on giving smallholder farmers from underserved
+                    communities access to a large network of global and
+                    local vendors market</p>
                 
         </div>
-        <div id='image' className='w-[300px] mx-auto'>
+        <div id='image' className='my-6 w-[300px] mx-auto'>
           <img src={union} alt='union'/>
         </div>
-          <div id='round-image' className='mt-4 w-[200px] mx-auto flex justify-between'>
+          <div id='round-image' className='mt-6 w-[200px] mx-auto flex justify-between'>
           <span>
             <img className='h-[40px]' src={img1} alt='image1'/>
           </span>
@@ -75,12 +67,12 @@ const Intro = () => {
             <img className='h-[40px]' src={img3} alt='image3'/>
           </span>
         </div>
-        <div className='mt-6 flex justify-between w-[330px] mx-auto'>
+        <div className='mt-8 mb-6 flex justify-between w-[300px] mx-auto'>
             <span>
-            <button className='h-[40px] w-[150px] bg-[#43a047] border rounded-bl-[30px] rounded-tr-[30px] text-gray-100'>Vendors</button>
+            <button className='h-[40px] w-[135px] bg-[#43a047] border rounded-bl-[18px] rounded-tr-[18px] text-gray-100'>Vendors</button>
             </span>
             <span>
-              <button className='h-[40px] w-[150px] bg-white border border-[#43a047] rounded-bl-[30px] rounded-tr-[30px] text-[#43a047]'>Farmers</button>
+              <button className='h-[40px] w-[135px] bg-white border border-[#43a047] rounded-bl-[18px] rounded-tr-[18px] text-[#43a047]'>Farmers</button>
             </span>
         </div>
       </div>
